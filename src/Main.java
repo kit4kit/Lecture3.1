27b339623da1542f03cb5a161dc0cfcdc7c89bd9
Trofimov Nikita <trofimov1nv@gmail.com>
2020-11-01 12:25:39 +0300

    Init Lecture3.1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketKopecks = 699900;
+        int bonus = ticketKopecks / 100 / 20;
+        System.out.println(bonus);
+
+    }
+}
