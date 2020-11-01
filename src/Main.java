e3b26e75969651fa8112c1a7e88c57da8de6356d
Trofimov Nikita <trofimov1nv@gmail.com>
2020-11-01 12:33:39 +0300

    Edit readme

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketKopecks = 699900;
         int bonus = ticketKopecks / 100 / 20;
+        System.out.printf("You bonus: ");
         System.out.println(bonus);
-
     }
 }
