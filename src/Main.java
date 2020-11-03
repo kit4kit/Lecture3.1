d0de06b97680f27a177f15540465480b222e0315
Trofimov Nikita <trofimov1nv@gmail.com>
2020-11-03 10:37:29 +0300

    Added method BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketKopecks = 699900;
-        int bonus = ticketKopecks / 100 / 20;
-        System.out.printf("You bonus: ");
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
